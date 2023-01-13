f238f4e4a88d798d2c3f17d77561018cd1370093
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-01-13 21:13:32 +0530

    new file:   .gitignore 	new file:   FF/code-pen 	new file:   README.md 	new file:   dontNeed/favicon.ico 	new file:   dontNeed/logo192.png 	new file:   dontNeed/logo512.png 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   public/logo192.png 	new file:   public/logo512.png 	new file:   public/manifest.json 	new file:   public/robots.txt 	new file:   src/components/App.js 	new file:   src/components/Editor.js 	new file:   src/hooks/useLocalStorage.js 	new file:   src/index.css 	new file:   src/index.js

---
+++ src/components/App.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+import Editor from './Editor'
+import useLocalStorage from '../hooks/useLocalStorage'
+
+function App() {
+  const [html, setHtml] = useLocalStorage('html', '')
+  const [css, setCss] = useLocalStorage('css', '')
+  const [js, setJs] = useLocalStorage('js', '')
+  const [srcDoc, setSrcDoc] = useState(`
+  <html>
+  <body style="display:flex; align-items:center; justify-content:center;">
+    <h1 style="color:green;">Project By Rakib RsM</h1>
+  </body>
+</html>`)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
+        </html>
+      `)
+    }, 250)
+
+    return () => clearTimeout(timeout)
+  }, [html, css, js])
+
+  return (
+    <>
+      <div className="pane top-pane">
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          language="javascript"
+          displayName="JS"
+          value={js}
+          onChange={setJs}
+        />
+      </div>
+      <div className="pane">
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </>
+  )
+}
+
+export default App;
